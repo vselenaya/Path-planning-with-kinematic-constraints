ed4a0e77c4460517f6390712a088a547c8b7a599
vselenaya <71250580+vselenaya@users.noreply.github.com>
2024-06-11 23:31:44 +0300

    Delete Experiments/cpp-effective-solution directory

--- Experiments/cpp-effective-solution/include/rassert.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define DEBUG
-
-#ifdef DEBUG
-    #define rassert(condition, info) if (!(condition)) { throw std::runtime_error("Assertion failed!\n" "Add info: " info); }
-#else
-    #define rassert(condition, info) ;
-#endif
